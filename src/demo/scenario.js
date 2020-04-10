7e2374c4d3f049665ab151dc8305043f34a78888
Louis Singer <louis.singer.git@gmail.com>
2020-04-10 10:05:06 +0200

    update scenario

--- src/demo/scenario.js
+++ src/demo/scenario.js
@@ -4,6 +4,7 @@
  */
 
 import Asset from '../asset.class'
+import { newKeyPairs } from '../merkleTree/utils'
 
 /**
  * Represent a public registry using to record all the transactions.
@@ -21,21 +22,23 @@ function newTransaction (from, to, amount, asset) {
   transactions.push({ from, to, amount, assetRootHash: asset.node.data })
 }
 
+// create users
 const doctorAccount = 'doctor'
+const doctorKeyPairs = newKeyPairs()
 const patientAccountA = 'patientA'
 const patientAccountB = 'patientB'
 
 // Create the assets object
-const dolipranePill = new Asset({ is: 'pill', type: 'doliprane 500mg' })
-const aspirinePill = new Asset({ is: 'pill', type: 'aspirine 120g' })
+const dolipranePill = new Asset({ is: 'pill', type: 'doliprane 500mg' }, Date.now(), doctorKeyPairs.privateKey)
+const aspirinePill = new Asset({ is: 'pill', type: 'aspirine 120g' }, Date.now, doctorKeyPairs.privateKey)
 
 // create the boxes: PUBLIC INFORMATIONS
-const boxDolipranes = new Asset({ is: 'box', id: 'dolipraneBox' }, Array(10).fill(dolipranePill))
+const boxDolipranes = new Asset({ is: 'box', id: 'dolipraneBox' }, Date.now(), doctorKeyPairs.privateKey, Array(10).fill(dolipranePill))
 // boxDolipranes.node.print()
-const boxAspirines = new Asset({ is: 'box', id: 'aspirineBox' }, Array(10).fill(aspirinePill))
+const boxAspirines = new Asset({ is: 'box', id: 'aspirineBox' }, Date.now(), doctorKeyPairs.privateKey, Array(10).fill(aspirinePill))
 // boxAspirines.node.print()
 
-const doctorBatch = new Asset({ is: 'batch', id: 'batch0' }, [boxDolipranes, boxAspirines])
+const doctorBatch = new Asset({ is: 'batch', id: 'batch0' }, Date.now(), doctorKeyPairs.privateKey, [boxDolipranes, boxAspirines])
 doctorBatch.node.print()
 
 // MODELIZE THE TRANSACTIONS
@@ -43,4 +46,4 @@ newTransaction(doctorAccount, patientAccountB, 1, dolipranePill)
 newTransaction(doctorAccount, patientAccountA, 9, dolipranePill)
 newTransaction(doctorAccount, patientAccountA, 1, boxAspirines)
 
-console.log(transactions.map(t => `${t.from} -> ${t.to} [${t.amount} ${t.assetRootHash}]`))
+// console.log(transactions.map(t => `${t.from} -> ${t.to} [${t.amount} ${t.assetRootHash}]`))
