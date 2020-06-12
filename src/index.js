9fe32f692c8417b5c04422f35dd81768d95554b8
Louis Singer <louis.singer.git@gmail.com>
2020-06-12 09:14:03 +0200

    update main module

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import { newKeyPairs } from './merkleTree/utils'
-import { newAsset, getRoot } from './asset'
+import * as asset from './asset'
+import Node from './merkleTree/Node.class'
+import * as utils from './merkleTree/utils'
+import * as hashUtils from './hash.utils'
 
-console.log('creating objects...')
-
-// a user represented as a keypair.
-const user = newKeyPairs()
-
-const pill0 = newAsset({ name: 'pill', type: 'doliprane 500mg' }, user.privateKey)
-const pill1 = newAsset({ name: 'pill', type: 'doliprane 500mg' }, user.privateKey)
-
-const pills = [pill0, pill1]
-
-const batch = newAsset({ name: 'batch', serialNumber: 'xz2' }, user.privateKey, pills)
-
-getRoot(pill0).print()
-getRoot(batch).print()
+export default {
+  ...asset,
+  Node,
+  ...utils,
+  ...hashUtils
+}
